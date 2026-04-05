2a387ba4bebdcda86ae4f841eef043f20e50403f
Дарина Мориконь <daryna.morykon@oa.edu.ua>
2026-04-06 02:14:09 +0300

    Add ProductsController

---
+++ Solemates/StepStyle.Web/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StepStyle.Web.Models;
+using StepStyle.Web.Repositories.Interfaces;
+
+namespace StepStyle.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IGenericRepository<Product> _productRepository;
+
+        public ProductsController(IGenericRepository<Product> productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var products = await _productRepository.GetAllAsync();
+
+            return View(products);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var product = await _productRepository.GetByIdAsync(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return View(product);
+        }
+    }
+}
