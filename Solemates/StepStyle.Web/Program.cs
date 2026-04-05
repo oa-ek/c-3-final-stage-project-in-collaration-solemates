66e3250db721903c7f3131e80e4e216848b13448
Дарина Мориконь <daryna.morykon@oa.edu.ua>
2026-04-06 02:08:04 +0300

    Add generic repository pattern implementation

--- Solemates/StepStyle.Web/Program.cs
+++ Solemates/StepStyle.Web/Program.cs
@@ -1,19 +1,20 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using StepStyle.Web.Data;
+using StepStyle.Web.Repositories.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString)); // Змінено UseSqlServer на UseSqlite
+    options.UseSqlite(connectionString)); 
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(StepStyle.Web.Repositories.GenericRepository<>));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
