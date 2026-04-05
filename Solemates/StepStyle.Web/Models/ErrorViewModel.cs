d6693b09c82963b67fd87788fca60688438b36d6
Дарина Мориконь <daryna.morykon@oa.edu.ua>
2026-04-05 21:12:50 +0300

    Initial commit: Base MVC project StepStyle

---
+++ Solemates/StepStyle.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StepStyle.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
